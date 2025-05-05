4fd07d572068b55c5ba7f1d334943f7157bd1b00
Boti Lóránt Ferenc <botil22@ady-nagyatad.sulinet.hu>
2025-05-05 15:01:48 +0200

    05.05 Lori

---
+++ Kvizjatek/Kerdeskezel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Kvizjatek
+{
+    internal class Kerdeskezel
+    {
+        public string kerdes;
+        public string valasz1;
+        public string valasz2;
+        public string valasz3;
+        public string valasz4;
+        public string helyesvalasz;
+        public int penz;
+
+        public Kerdeskezel(string sor)
+        {
+            string[] vag = sor.Split(';');
+            this.kerdes = vag[0];
+            this.valasz1 = vag[1];
+            this.valasz2 = vag[2];
+            this.valasz3 = vag[3];
+            this.valasz4 = vag[4];
+            this.helyesvalasz = vag[5];
+            this.penz = Convert.ToInt32(vag[6]);
+        }
+    }
+}
--- Kvizjatek/MainWindow.xaml.cs
+++ Kvizjatek/MainWindow.xaml.cs
@@ -8,21 +8,55 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace Kvizjatek
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
 
+		List<Kerdeskezel> kerdesek = new List<Kerdeskezel>();
+        List<string> valaszok = new List<string>();
         private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			string kerdes = "";
+
+
+			for (int i = 0; i < kerdesek.Count; i++)
+			{
+				kerdes = kerdesek[i].Split(";")[0];
+
+			}
+
+			Kerdes.Text = kerdes;
+		}
+
+
+		private void kviz_Loaded(object sender, RoutedEventArgs e)
         {
+            string[] betolt = File.ReadAllLines("kerdes.txt", Encoding.UTF8);
+            for (int i = 1; i < betolt.Length; i++)
+            {
+                kerdesek.Add(new Kerdeskezel();
+
+            }
+				
+			for (int i = 0; i < kerdesek.Count(); i++)
+			{
+				valaszok.Add(kerdesek[i].Split(";")[1]);
+			}
+
+            Random rand = new Random();
+
+			valasz1.Content = kerdesek[0];
+			valasz2.Content = kerdesek[1];
 
         }
     }
